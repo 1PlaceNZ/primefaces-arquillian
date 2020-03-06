c51dd1d99b2fc7316471f43eda4ac609d528c6a5
Darrell Wu <darrell@1placeonline.com>
2020-03-07 10:13:57 +1300

    Fixed up style check errors

--- primefaces-arquillian/src/main/java/org/primefaces/extensions/arquillian/component/SelectCheckboxMenu.java
+++ primefaces-arquillian/src/main/java/org/primefaces/extensions/arquillian/component/SelectCheckboxMenu.java
@@ -28,13 +28,13 @@ import org.primefaces.extensions.arquillian.component.base.AbstractInputComponen
 import org.primefaces.extensions.arquillian.extension.findby.FindByParentPartialId;
 
 public abstract class SelectCheckboxMenu extends AbstractInputComponent {
-	
-	@FindBy(className = "ui-selectcheckboxmenu-trigger")
-	private WebElement panelTrigger;
+
+    @FindBy(className = "ui-selectcheckboxmenu-trigger")
+    private WebElement panelTrigger;
 
     @FindBy(className = "ui-selectcheckboxmenu-token-label")
     private List<WebElement> selectedItems;
-    
+
     @FindBy(className = "ui-selectcheckboxmenu-token-icon")
     private List<WebElement> selectedItemsRemoveIcon;
 
@@ -43,40 +43,39 @@ public abstract class SelectCheckboxMenu extends AbstractInputComponent {
 
     @FindByParentPartialId(value = "_panel", searchFromRoot = true)
     private WebElement panel;
-    
-    
+
     public List<String> getOptionLabels() {
         List<String> result = new ArrayList<>();
         List<WebElement> options = panel.findElements(By.tagName("li"));
         for (WebElement option : options) {
-        	WebElement labelElement = option.findElement(By.tagName("label"));
+            WebElement labelElement = option.findElement(By.tagName("label"));
             result.add(labelElement.getAttribute("textContent")); //getText can only be used on visible elements
         }
         return result;
     }
-    
+
     public boolean containOption(String optionLabel) {
         List<WebElement> options = panel.findElements(By.tagName("li"));
         for (WebElement option : options) {
-        	String label = option.findElement(By.tagName("label")).getAttribute("textContent"); //getText can only be used on visible elements
+            String label = option.findElement(By.tagName("label")).getAttribute("textContent"); //getText can only be used on visible elements
             if (optionLabel.equals(label)) {
                 return true;
             }
         }
         return false;
     }
-    
+
     public void toggleDropdown() {
         if (panel.isDisplayed()) {
-        	panelTrigger.click();
+            panelTrigger.click();
             PrimeGraphene.waitGui().until(PrimeExpectedConditions.invisibleAndAnimationComplete(panel));
         }
         else {
-        	panelTrigger.click();
+            panelTrigger.click();
             PrimeGraphene.waitGui().until(PrimeExpectedConditions.visibileAndAnimationComplete(panel));
         }
     }
-    
+
     public void select(String label) {
         if (isSelected(label)) {
             return;
@@ -86,16 +85,16 @@ public abstract class SelectCheckboxMenu extends AbstractInputComponent {
         toggleSelection(label);
         hidePanel();
     }
-    
+
     public void unselect(String label) {
-    	if (!isSelected(label)) {
+        if (!isSelected(label)) {
             return;
         }
-    	showPanel();
+        showPanel();
         toggleSelection(label);
         hidePanel();
     }
-    
+
     public void selectMultiple(String label) {
         if (isSelected(label)) {
             return;
@@ -104,25 +103,25 @@ public abstract class SelectCheckboxMenu extends AbstractInputComponent {
         toggleSelection(label);
         hidePanel();
     }
-    
+
     public void unselectAll() {
-    	if(selectedItemsRemoveIcon != null) {
-    		selectedItemsRemoveIcon.forEach( item -> item.click());
-    	}
+        if (selectedItemsRemoveIcon != null) {
+            selectedItemsRemoveIcon.forEach( item -> item.click());
+        }
     }
-    
+
     // to keep compatibility with selectonemenu
     public String getSelectedLabel() {
-        return selectedItems!= null && 
-        		!selectedItems.isEmpty() ? selectedItems.get(0).getText() : "";
+        return selectedItems != null &&
+                !selectedItems.isEmpty() ? selectedItems.get(0).getText() : "";
     }
-    
+
     public List<String> getSelectedLabels() {
-    	List<String> texts = new ArrayList<String>();
-        if (selectedItems!= null && !selectedItems.isEmpty()) {
-        	texts = selectedItems.stream().map(label -> label.getText()).collect(Collectors.toList());
+        List<String> texts = new ArrayList<String>();
+        if (selectedItems != null && !selectedItems.isEmpty()) {
+            texts = selectedItems.stream().map(label -> label.getText()).collect(Collectors.toList());
         }
-        return texts; 
+        return texts;
     }
 
     public boolean isSelected(String label) {
@@ -135,29 +134,29 @@ public abstract class SelectCheckboxMenu extends AbstractInputComponent {
     }
 
     protected void click(WebElement element) {
-    	//cannot click on input -> "element not interactable" exception
-    	element.findElement(By.className("ui-chkbox")).click();
+        // cannot click on input -> "element not interactable" exception
+        element.findElement(By.className("ui-chkbox")).click();
     }
-    
+
     private void showPanel() {
-    	if (!panel.isDisplayed()) {
+        if (!panel.isDisplayed()) {
             toggleDropdown();
         }
     }
-    
+
     private void hidePanel() {
-    	if (panel.isDisplayed()) {
+        if (panel.isDisplayed()) {
             toggleDropdown();
         }
     }
-    
+
     private void toggleSelection(String label) {
-    	panel.findElements(By.tagName("li")).forEach(element -> {
-        	String labelItem = element.findElement(By.tagName("label")).getText();
+        panel.findElements(By.tagName("li")).forEach(element -> {
+            String labelItem = element.findElement(By.tagName("label")).getText();
             if (labelItem.equalsIgnoreCase(label)) {
                 click(element);
             }
         });
     }
-    
+
 }
